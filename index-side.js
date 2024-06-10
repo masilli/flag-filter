f61d12c60467e0f567af2086767d82566110c29e
masilli <masilli@gmail.com>
2024-06-10 13:46:23 +0100

    added more symbol categories and wikipedia link

--- index-side.js
+++ index-side.js
@@ -1,15 +1,15 @@
-import { countriesArray } from "./countries.js"
+import { countriesArray } from "./countries.js";
 
-const contDiv = document.getElementById("content")
-const totalResults = document.getElementById("total-results")
+const contDiv = document.getElementById("content");
+const totalResults = document.getElementById("total-results");
 
-const filterDash = document.getElementById("filter-dashboard")
+const filterDash = document.getElementById("filter-dashboard");
 
 //const chosenContinents = document.getElementById("continent-options")
 
 //console.log(countriesArray)
 
-let matchingFlags = countriesArray
+let matchingFlags = countriesArray;
 
 // filterDash.addEventListener('submit', function (e) {
 //     e.preventDefault()
@@ -24,46 +24,46 @@ let matchingFlags = countriesArray
 // })
 
 filterDash.addEventListener("submit", function (e) {
-  e.preventDefault()
+  e.preventDefault();
 
   const selectedContinents = [
     ...document.querySelectorAll(
       '#continent-options input[type="checkbox"]:checked'
     ),
-  ].map((checkbox) => checkbox.value)
+  ].map((checkbox) => checkbox.value);
   const selectedColors = [
     ...document.querySelectorAll(
       '#color-options input[type="checkbox"]:checked'
     ),
-  ].map((checkbox) => checkbox.value)
+  ].map((checkbox) => checkbox.value);
 
   if (selectedContinents.length === 0 || selectedColors.length === 0) {
     //console.log("empty: "+selectedContinents)
-    matchingFlags = []
+    matchingFlags = [];
   } else {
     matchingFlags = countriesArray.filter((country) => {
       let includeContinents =
         selectedContinents.length === 0 ||
-        selectedContinents.includes(country.continent)
+        selectedContinents.includes(country.continent);
       let includeColors =
         selectedColors.length === 0 ||
-        selectedColors.some((color) => country.colors.includes(color))
-      return includeContinents && includeColors
-    })
+        selectedColors.some((color) => country.colors.includes(color));
+      return includeContinents && includeColors;
+    });
   }
-  renderFlags()
-})
+  renderFlags();
+});
 
-renderFlags()
+renderFlags();
 
 function renderFlags() {
-  contDiv.innerHTML = ""
+  contDiv.innerHTML = "";
 
-  totalResults.textContent = matchingFlags.length
+  totalResults.textContent = matchingFlags.length;
 
   matchingFlags.forEach((region) => {
-    const countryNameElement = document.createElement("p")
-    countryNameElement.classList.add("country")
+    const countryNameElement = document.createElement("p");
+    countryNameElement.classList.add("country");
     // countryNameElement.innerHTML = `<img src="${region.flag}"><br><span class="region-name">${region.name}</span>`
     countryNameElement.innerHTML = `
     <figure>
@@ -71,41 +71,45 @@ function renderFlags() {
         <figcaption>
         <span class="region-name">${region.name}</span>
         </figcaption>
-    </figure>`
+    </figure>`;
 
     // myModal
-  const modal = document.getElementById('myModal');
-  const span = document.getElementsByClassName('close')[0];
-  const modalCountryName = document.getElementById('modal-country-name');
-  const modalCountryFlag = document.getElementById('modal-country-flag');
-  const modalCountryCapital = document.getElementById('modal-country-capital');
-  const modalCountryContinent = document.getElementById('modal-country-continent');
-  const modalCountryColors = document.getElementById('modal-country-colors');
+    const modal = document.getElementById("myModal");
+    const span = document.getElementsByClassName("close")[0];
+    const modalCountryName = document.getElementById("modal-country-name");
+    const modalCountryFlag = document.getElementById("modal-country-flag");
+    const modalCountryCapital = document.getElementById(
+      "modal-country-capital"
+    );
+    const modalCountryContinent = document.getElementById(
+      "modal-country-continent"
+    );
+    const modalCountryColors = document.getElementById("modal-country-colors");
+    const modalCountryWiki = document.getElementById("modal-country-wikipedia");
 
-  countryNameElement.querySelector('figure').addEventListener('click', () => {
-    const country = matchingFlags.find(c => c.id === region.id);
-    if (country) {
-      modalCountryName.textContent = `Name: ${country.name}`;
-      modalCountryFlag.src = `${country.flag}`;
-      modalCountryCapital.textContent = `Capital: ${country.capital}`;
-      modalCountryContinent.textContent = `Continent: ${country.continent}`;
-      modalCountryColors.textContent = `Colors: ${country.colors.join(', ')}`;
-      modal.style.display = 'block';
-    }
+    countryNameElement.querySelector("figure").addEventListener("click", () => {
+      const country = matchingFlags.find((c) => c.id === region.id);
+      if (country) {
+        modalCountryName.textContent = country.name;
+        modalCountryFlag.src = `${country.flag}`;
+        modalCountryCapital.textContent = `Capital: ${country.capital}`;
+        modalCountryContinent.textContent = `Continent: ${country.continent}`;
+        // modalCountryColors.textContent = `Colors: ${country.colors.join(", ")}`;
+        modalCountryWiki.innerHTML = `<span>More information</span>: <a href="https://en.wikipedia.org/wiki/Flag_of_${country.name}" target="_blank">wikipedia</a>`;
+        modal.style.display = "block";
+      }
+    });
+
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    };
+
+    contDiv.appendChild(countryNameElement);
   });
-
-  span.onclick = function() {
-    modal.style.display = 'none';
-  }
-
-  window.onclick = function(event) {
-    if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-  }
-
-    contDiv.appendChild(countryNameElement)
-  })
-  
-
 }
